aa72ba0bb3d88eb2f8a204b844a25e859cf31c4e
jerrynim <tjerry3@naver.com>
2024-03-21 19:28:53 +0900

    sample pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,48 @@
+import { http, HttpResponse, passthrough } from "msw";
+import { setupWorker } from "msw/browser";
+import { useEffect } from "react";
+import axios from "axios";
+
+const items = Array(24)
+    .fill(null)
+    .map((_, index) => ({
+        text: `item${index + 1}`,
+    }));
+
+// Describe the network.
+const handlers = [
+    http.get("/items", ({ request }) => {
+        const url = new URL(request.url);
+        const page = Number(url.searchParams.get("page"));
+
+        if (!page) {
+            return HttpResponse.json("page is required", { status: 400 });
+        }
+        return HttpResponse.json({
+            items: items.splice((page - 1) * 10, 10),
+        });
+    }),
+    http.get("/items/max-length", ({ params }) => {
+        return HttpResponse.json({
+            "max-length": 24,
+        });
+    }),
+];
+
+const worker = setupWorker(...handlers);
+await worker.start();
 const App = () => {
+    useEffect(() => {
+        axios.get("/items?page=3").then((res) => {
+            console.log(res, "res");
+        });
+    }, []);
+    useEffect(() => {
+        axios.get("/items/max-length").then((res) => {
+            console.log(res, "max-length");
+        });
+    }, []);
+
     return (
         <div>
             <Pagination />
@@ -9,3 +53,5 @@ const App = () => {
 const Pagination = () => {
     return <div id="pagination"></div>;
 };
+
+export default App;
